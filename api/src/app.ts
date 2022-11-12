093a91caf75f5d4acde0b34f3e77693430ad2a7d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2022-11-11 22:47:35 -0300

    feat: added error handler

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import 'express-async-errors';
 import 'dotenv/config';
 
 import { uploadRoutes } from './routes/upload.routes';
 import { setupDatabase } from './database/setup';
+import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
 
@@ -12,5 +14,7 @@ app.use(morgan('dev'));
 app.use(setupDatabase);
 
 app.use(uploadRoutes);
+
+app.use(errorHandler);
 
 export { app };
---
+++ api/src/middlewares/errorHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { APIError } from "../errors/APIError";
+
+export function errorHandler(err: Error | APIError | any, req: Request, res: Response, next: NextFunction) {
+  if (err) {
+    if (err instanceof APIError) {
+      return res.status(err.statusCode).json({ message: err.message });
+    }
+
+    console.error('#### An unexpected error was returned ####');
+    console.error(err);
+
+    return res.status(500).json({ message: 'Houve um erro inesperado no nosso servidor, tente novamente mais tarde.' });
+  }
+}
